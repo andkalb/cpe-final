b39b6bdeb69818d17aac3adf1693b4f649134701
andkalb <89561907+andkalb@users.noreply.github.com>
2022-11-18 18:33:27 -0800

    Created Repository and Base Files

---
+++ cpe-final.ino
@@ -0,0 +1,36 @@
+// CPE 301 Final Project - Fall 2022
+// Written by Andrew Kalb and Preston Peterson
+//
+
+
+//
+// ADDRESSES/PORT SETUP
+//
+
+//
+//
+//
+
+
+//
+// SETUP CODE
+//
+void setup() 
+{
+
+}
+//
+//
+//
+
+
+//
+// LOOP CODE
+//
+void loop() 
+{
+
+}
+//
+//
+//
---
+++ helper_functions.ino
@@ -0,0 +1,15 @@
+// helper_functions.ino
+// This file contains various
+// helper functions defined for the
+// convenience of the overall program.
+
+
+void write(unsigned int* port, unsigned int pinNumber)
+{
+
+}
+
+bool read(unsigned int* port, unsigned int pinNumber)
+{
+
+}
